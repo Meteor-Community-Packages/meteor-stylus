b9b13b8d9a65f2cb8306064c2c647f47bea8cef4
Frederick Stark <coagmano@gmail.com>
2017-03-28 17:03:48 +1100

    Modified plugin to support plugins from mquandalle:stylus

---
+++ package.js
@@ -0,0 +1,36 @@
+Package.describe({
+  name: 'coagmano:stylus',
+  version: '1.0.0',
+  summary: 'Stylus plugin with plugins from mquandalle:stylus. Compatible with Meteor 1.4 and \'ecmascript\''
+});
+
+Package.registerBuildPlugin({
+  name: 'compileStylusBatch',
+  use: ['ecmascript', 'caching-compiler'],
+  sources: [
+    'plugin/compile-stylus.js'
+  ],
+  npmDependencies: {
+    stylus: 'https://github.com/meteor/stylus/tarball/bb47a357d132ca843718c63998eb37b90013a449', // fork of 0.54.5
+    nib: '1.1.2',
+    jeet: '7.1.0',
+    rupture: '0.6.2',
+    axis: '0.4.3',
+    typographic: '2.9.3',
+    'autoprefixer-stylus': '0.13.0'
+  }
+});
+
+Package.onUse(function (api) {
+  api.use('isobuild:compiler-plugin@1.0.0');
+});
+
+Package.onTest(function (api) {
+  api.use(['tinytest', 'coagmano:stylus', 'test-helpers', 'templating']);
+  api.addFiles([
+    'stylus_tests.html',
+    'stylus_tests.styl',
+    'stylus_tests.import.styl',
+    'stylus_tests.js'
+  ],'client');
+});
