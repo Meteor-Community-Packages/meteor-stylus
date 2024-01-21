24d2fa40dd447ba13c232f7298cffd59f26c9ec6
Manuel Timita <manuel@illustreets.com>
2024-01-21 20:01:11 +0000

    Suppressed "Accessing non-existent property ... in exports" warnings

--- package.js
+++ package.js
@@ -12,7 +12,18 @@ Package.registerBuildPlugin({
     'plugin/compile-stylus.js'
   ],
   npmDependencies: {
-    stylus: 'https://github.com/meteor/stylus/tarball/bb47a357d132ca843718c63998eb37b90013a449', // fork of 0.54.5
+    // stylus: 'https://github.com/meteor/stylus/tarball/bb47a357d132ca843718c63998eb37b90013a449', // fork of 0.54.5
+
+    // This one is a fork of 0.54.5, but with the following warnings suppressed:
+    // (node:52236) Warning: Accessing non-existent property 'lineno' of module exports inside circular dependency
+    // (Use `node --trace-warnings ...` to show where the warning was created)
+    // (node:52236) Warning: Accessing non-existent property 'column' of module exports inside circular dependency
+    // (node:52236) Warning: Accessing non-existent property 'filename' of module exports inside circular dependency
+    // (node:52236) Warning: Accessing non-existent property 'lineno' of module exports inside circular dependency
+    // (node:52236) Warning: Accessing non-existent property 'column' of module exports inside circular dependency
+    // (node:52236) Warning: Accessing non-existent property 'filename' of module exports inside circular dependency
+    stylus: 'https://github.com/manueltimita/stylus/tarball/6eabf4db170b962a244243e0992ccca3b740dc7f',
+
     nib: '1.1.2',
     jeet: '7.1.0',
     rupture: '0.6.2',
